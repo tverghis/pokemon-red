9645f292d8a48b0f4eaefaf8d228249405e3dba3
Tarun Verghis <tarun.verghis@gmail.com>
2019-11-25 21:25:06 -0800

    Initial script to fetch data

---
+++ scripts/load_data.py
@@ -0,0 +1,22 @@
+import requests
+import sys
+
+pokemon_data = []
+for id in range(1, 152): # non-inclusive range end
+    response = requests.get("https://pokeapi.co/api/v2/pokemon/{}".format(id))
+
+    if response.status_code != 200:
+        print("Request failed")
+        sys.exit()
+
+    response_json = response.json()
+    moves = []
+
+    for move in response_json['moves']:
+        for detail in move['version_group_details']:
+            if detail['version_group']['name'] == 'red-blue':
+                moves.append(move['move']['name'])
+
+    pokemon_data.append({ "id": id, "name": response_json['name'], "moves": moves })
+
+print(pokemon_data)
